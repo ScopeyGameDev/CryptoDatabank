25d39e269db11e60babb84aa21d34428a1c204e2
ScopeyGameDev <57968756+ScopeyGameDev@users.noreply.github.com>
2021-05-20 22:13:54 +1000

    Buy/Sell/Change

---
+++ Assets/Scripts/Cryptocurrency.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum EnumBuySell { Buy, Sell };
+[System.Serializable]
+public class BuySellCrypto
+{
+    public EnumBuySell BuySell;
+    public float Price;
+    public float Value;
+
+    public BuySellCrypto(EnumBuySell _BuySell, float _Price, float _Value)
+	{
+        BuySell = _BuySell;
+        Price = _Price;
+        Value = _Value;
+	}
+}
+
+[System.Serializable]
+public class Cryptocurrency
+{
+    public string Name;
+    public float Balance;
+    public float Price;
+    public float Profit
+	{
+        get { return CalculateProfits(); }
+	}
+
+    public List<BuySellCrypto> BuySellCryptos = new List<BuySellCrypto>();
+
+    void BuySell(EnumBuySell BuySell, float _Price, float _Value)
+	{
+        BuySellCrypto BuySellCrypto = new BuySellCrypto(BuySell, _Price, _Value);
+        BuySellCryptos.Add(BuySellCrypto);
+        Balance = _Price;
+	}
+
+    public void ChangePrice(float _Price)
+	{
+        Price = _Price;
+	}
+
+    float CalculateProfits()
+	{
+        float Spent = 0;
+
+		foreach (BuySellCrypto item in BuySellCryptos)
+		{
+            if (item.BuySell == EnumBuySell.Buy)
+			{
+                Spent += item.Value;
+			}
+			else
+			{
+                Spent -= item.Value;
+			}
+		}
+
+        float CurrentValue = Balance * Price;
+
+        return CurrentValue - Spent;
+	}
+
+    void CreateCryptocurrency(string _Name, float _Balance, float _Price)
+	{
+        Name = _Name;
+        Balance = _Balance;
+        Price = _Price;
+	}
+}
---
+++ Assets/Scripts/CryptocurrencyManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CryptocurrencyManager : MonoBehaviour
+{
+    [SerializeField] List<Cryptocurrency> Cryptocurrencies = new List<Cryptocurrency>();
+
+    [SerializeField] TMP_Text TitleText;
+
+    [SerializeField] TMP_Text BalanceText;
+    [SerializeField] TMP_Text PriceText;
+    [SerializeField] TMP_Text ProfitText;
+
+    [SerializeField] TMP_InputField BalanceInputField;
+    [SerializeField] TMP_InputField PriceInputField;
+
+    [SerializeField] Button ChangeButton;
+    [SerializeField] Button BuyButton;
+    [SerializeField] Button SellButton;
+
+    void Start()
+    {
+        if (Cryptocurrencies.Count == 0)
+            return;
+
+        UpdateUI();
+
+        ChangeButton.onClick.AddListener(Change);
+    }
+
+    void UpdateUI()
+	{
+        TitleText.text = Cryptocurrencies[0].Name;
+        BalanceText.text = Cryptocurrencies[0].Balance.ToString();
+        PriceText.text = Cryptocurrencies[0].Price.ToString("$0.00");
+        ProfitText.text = Cryptocurrencies[0].Profit.ToString("$0.00");
+    }
+
+    void Change()
+	{
+        float BalanceInput = float.Parse(BalanceInputField.text);
+        float PriceInput = float.Parse(PriceInputField.text);
+        Cryptocurrencies[0].Balance = BalanceInput;
+        Cryptocurrencies[0].Price = PriceInput;
+        UpdateUI();
+	}
+}
